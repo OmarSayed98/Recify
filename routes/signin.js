305b3e06bb9c605024896c62340d12c76075a7ac
Dijkstra <osayed505@gmail.com>
2020-03-29 06:16:25 +0200

    signin

--- routes/signin.js
+++ routes/signin.js
@@ -1,7 +1,29 @@
 const express=require('express');
 const router=express.Router();
 const mongoose=require('../server.js');
+const users=require('../models/users');
 router.get('/',function(req,res){
     res.render('sign_in');
+});
+router.post('/',function(req,res){
+    var data=req.body;
+    users.findOne({email:data.UserEmail},function(err,user){
+        if(err)
+            throw err;
+        if(user) {
+            user.comparePassword(data.UserPassword, function (err, match) {
+                if (err)
+                    throw err;
+                if (match)
+                    res.render('sign_in');
+                else{
+
+                }
+            })
+        }
+        else{
+
+        }
+    })
 });
 module.exports=router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,29 +1,27 @@
 const express=require('express');
 const router=express.Router();
 const schema=require('../models/users');
-const error=require('../public/javascripts/sign_up');
 router.get('/',function(req,res){
     res.render('sign_up');
 });
-
 router.post('/',function(req,res){
     const data=req.body;
-    schema.find({email:data.UserEmail},function(err,document){
+    const email=data.UserEmail.toLowerCase();
+    schema.find({email:email},function(err,document){
         if(err)
             throw err;
         if(!document.length){
             const user=new schema({
                 name:data.Name,
                 password:data.Password,
-                email:data.UserEmail
+                email:email
             });
             user.save().then(function(){
                 res.render('sign_in');
             });
         }
         else{
-            error;
-            res.render('sign_up');
+
         }
     });
 });
