a8890675fdafca03049c3b04fb6958ac73724088
Dijkstra <osayed505@gmail.com>
2020-04-17 19:55:17 +0200

    fixed bug with sessions

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(session({
     secret:process.env.SECRET_KEY,
     resave:false,
     saveUninitialized:false,
+    cookie : {
+        maxAge: 1000* 60 * 60 *24 * 365
+    },
     store:new MongoStore({mongooseConnection:db})
 }));
 app.use(flash());
