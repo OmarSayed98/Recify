9b307efe68c3a20056ea6c481131b6884a23a811
Omar El Halawany <omar.el-halawany@hotmail.com>
2020-05-09 18:01:18 +0200

    iframe adjustment

--- public/javascripts/moviepageposter.js
+++ public/javascripts/moviepageposter.js
@@ -2,14 +2,17 @@ function play(trailer){
     var x = document.getElementById("hiddenTrailer");
     if (x.style.display !== "block") {
         x.style.display = "block";
-        x.innerHTML = '<iframe width="1280" height="720" id="theIframe" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        document.getElementById("theIframe").src=trailer;
+       x.innerHTML = '<iframe width="100%" height="480" id="theIframe" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+       document.getElementById("theIframe").src=trailer;
+    }
+    else {
+        x.style.display="none";
     }
 
 
 }
 
-$(document).click(function(e) {
+/*$(document).click(function(e) {
 
 
     if( e.target.id !== 'hiddenTrailer' && e.target.id!=="playbtn") {
@@ -17,4 +20,4 @@ $(document).click(function(e) {
             display: "none"
         });
     }
-});
+});*/
