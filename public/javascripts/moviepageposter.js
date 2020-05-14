e7d0477072d7aa3409bc706599c88ad7767cb9ab
Omar El Halawany <omar.el-halawany@hotmail.com>
2020-05-14 13:27:34 +0200

    implementing like/dislike frontend and sending request

--- public/javascripts/moviepageposter.js
+++ public/javascripts/moviepageposter.js
@@ -14,11 +14,68 @@ function play(trailer){
 
 function likeMovie()
 {
+    var xhttp = new XMLHttpRequest();
+    var likebtn=document.getElementById('like');
+    var dislikebtn=document.getElementById('dislike');
+    var url="";
+    if(!likebtn.classList.contains('likeClicked'))
+    {
+        if(dislikebtn.classList.contains('dislikeClicked'))
+        {
+            dislikebtn.classList.remove('dislikeClicked');
+            likebtn.classList.add('likeClicked');
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("");
+        }
+        else {
+            likebtn.classList.add('likeClicked');
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("");
+
+        }
+    }
+    else if(likebtn.classList.contains('likeClicked'))
+    {
+        likebtn.classList.remove('likeClicked');
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("");
+    }
 
 }
 
 function dislikeMovie()
 {
+    var likebtn=document.getElementById('like');
+    var dislikebtn=document.getElementById('dislike');
+    var url="";
+    var xhttp = new XMLHttpRequest();
+
+    if(!dislikebtn.classList.contains('dislikeClicked'))
+    {
+        if(likebtn.classList.contains('likeClicked'))
+        {
+            likebtn.classList.remove('likeClicked');
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("");
+        }
+        else {
+            dislikebtn.classList.add('dislikeClicked');
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("");
+        }
+    }
+    else if(dislikebtn.classList.contains('dislikeClicked'))
+    {
+        dislikebtn.classList.remove('dislikeClicked');
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("");
+    }
 
 }
 
