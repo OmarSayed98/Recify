c21c75e879ab0e6ffe25c76ff290fe9ada9146cf
Dijkstra <osayed505@gmail.com>
2020-05-17 21:42:30 +0200

    promise handled in mongoose connect

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ let db=mongoose.connection;
 mongoose.connect("mongodb://localhost:27017/Dijkstra", {
     useUnifiedTopology: true,
     useNewUrlParser: true
-});
+}).then(()=>console.log('connected to db'))
+    .catch(err=>console.log(err));
 module.exports=db;
