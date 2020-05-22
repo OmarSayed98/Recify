5eef5dc858b64c2d4119b46e962dfe8691cf7bfe
Dijkstra <osayed505@gmail.com>
2020-05-22 22:05:54 +0200

    speed improvment try

--- routes/homepage.js
+++ routes/homepage.js
@@ -40,8 +40,8 @@ router.get('/',redirect,(req,res)=>{
                     return {id:respid_tv.imdb_id,poster:`https://image.tmdb.org/t/p/w600_and_h900_bestv2${tv.poster_path}`};
                 });
             });
-            Promise.all(trending_id_tv).then((values)=>{
-                Promise.all(trending_id).then(values_movie=>{
+            Promise.all(trending_id).then((values_movie)=>{
+                Promise.all(trending_id_tv).then(values=>{
                     const user=req.session.name.split(' ')[0];
                     res.render('homepage',{name:user,imdbid_poster:values_movie,imdb_poster_tv:values});
                 })
