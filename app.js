5f179b9f9d5536d5229eaa8328aa536b9e9361b1
Dijkstra <osayed505@gmail.com>
2020-05-24 20:08:43 +0200

    adding upcoming to slider

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const route=require('./routes/index');
 const db=require('./server');
 const MongoStore=require('connect-mongo')(session);
 const ms=require('ms');
-const cron=require('node-cron');
-const trend=require('./models/trending');
-const request=require('request-promise');
+const schedule=require('./scheduledb');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -38,73 +36,7 @@ app.use(function(req,res,next){
 app.use(cookieParser());
 app.use('/public',express.static(path.join(__dirname, 'public')));
 route(app);
-const tmdb=cron.schedule('26 16 * * SAT',()=>{
-    const option={
-        url:"https://api.themoviedb.org/3/trending/movie/week?api_key=9bde952e56ff27d1016ff6144cbf27c9",
-        json:true
-    };
-    const optiontv={
-        url:"https://api.themoviedb.org/3/trending/tv/week?api_key=9bde952e56ff27d1016ff6144cbf27c9",
-        json:true
-    };
-    const topratedoptioin={
-        url:'https://api.themoviedb.org/3/movie/upcoming?api_key=9bde952e56ff27d1016ff6144cbf27c9&language=en-US&page=1',
-        json:true
-    };
-    request(option).then(resp=>{
-        let trending_id=[];
-        trending_id=resp.results.map((movie)=>{
-            const optionid={
-                url:`https://api.themoviedb.org/3/movie/${movie.id}/external_ids?api_key=9bde952e56ff27d1016ff6144cbf27c9`,
-                json:true
-            }
-            return request(optionid).then(respid=>{
-                return {id:respid.imdb_id,poster:`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`};
-            }).catch(err=>console.log(err));
-        });
-        request(optiontv).then(resptv=>{
-            let trending_id_tv=[];
-            trending_id_tv=resptv.results.map((tv)=>{
-                const optionid_tv={
-                    url:`https://api.themoviedb.org/3/tv/${tv.id}/external_ids?api_key=9bde952e56ff27d1016ff6144cbf27c9&language=en-US`,
-                    json:true
-                }
-                return request(optionid_tv).then(respid_tv=>{
-                    return {id:respid_tv.imdb_id,poster:`https://image.tmdb.org/t/p/w600_and_h900_bestv2${tv.poster_path}`};
-                });
-            });
-            request(topratedoptioin).then(upcoming=>{
-                let upcoming_data=[];
-                upcoming_data=upcoming.results.map((top)=>{
-                    const optionid_top={
-                        url:`https://api.themoviedb.org/3/movie/${top.id}/external_ids?api_key=9bde952e56ff27d1016ff6144cbf27c9`,
-                        json:true
-                    }
-                    return request(optionid_top).then(id_top=>{
-                        return {id:id_top.imdb_id,poster:`https://image.tmdb.org/t/p/w600_and_h900_bestv2${top.poster_path}`,data:top};
-                    });
-                });
-                Promise.all(trending_id).then((values_movie)=>{
-                    Promise.all(trending_id_tv).then(values=>{
-                        Promise.all(upcoming_data).then((values_top=>{
-                            const trending={
-                                trending_tv:values,
-                                trending_movie:values_movie,
-                                upcoming:values_top,
-                                name:'omar'
-                            };
-                            trend.findOneAndUpdate({name:'omar'},trending,{
-                                upsert:true,
-                                useFindAndModify: false
-                            }).then(()=>console.log('data updated'));
-                        })).catch(err=>console.log(err));
-                    }).catch(err=>console.log(err));
-                }).catch(err=>console.log(err));
-            }).catch(err=>console.log(err));
-        })
-    });
-});
-tmdb.start();
+schedule.start();
 app.get('/',(req,res)=>{
     res.redirect('/home');
 });
