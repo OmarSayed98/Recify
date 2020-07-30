9acb2b792282b45fbb6c28c57f6ebcb685986d85
Dijkstra <osayed505@gmail.com>
2020-07-30 23:14:01 +0200

    added name to notifications page

--- routes/notification.js
+++ routes/notification.js
@@ -2,9 +2,9 @@ const express=require('express');
 const router=express.Router();
 const user=require('../models/users');
 router.get('/',(req,res)=>{
-   user.findOne({id:req.session.user_id})
+   user.findOne({_id:req.session.user_id})
        .then(result=>{
-          res.render('notification',{notifications:result.notifications});
+          res.render('notification',{notifications:result.notifications,name:req.session.name});
        });
 });
 module.exports=router;
